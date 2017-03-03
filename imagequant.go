29c239a8105d80b897ece5be491cefcfef743db8
mappu <mappu04@gmail.com>
2017-03-03 18:16:40 +1300

    split up cgo CFLAGS into files with separate build tags

--- imagequant.go
+++ imagequant.go
@@ -21,8 +21,6 @@ import (
 )
 
 /*
-#cgo CFLAGS: -O3 -fno-math-errno -fopenmp -funroll-loops -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse -fexcess-precision=fast
-#cgo LDFLAGS: -fopenmp -static
 #include "libimagequant.h"
 
 const char* liqVersionString() {
