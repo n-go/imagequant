2d85b83a5edd49e614061151f2c1a61481d190ba
mappu <mappu04@gmail.com>
2017-03-05 11:50:13 +1300

    cflags: add -ldl to fix build on Ubuntu 16.04

--- cflags_linux.go
+++ cflags_linux.go
@@ -4,6 +4,6 @@ package imagequant
 
 /*
 #cgo CFLAGS: -O3 -fno-math-errno -fopenmp -funroll-loops -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse -fexcess-precision=fast
-#cgo LDFLAGS: -lm -fopenmp
+#cgo LDFLAGS: -lm -fopenmp -ldl
 */
 import "C"
